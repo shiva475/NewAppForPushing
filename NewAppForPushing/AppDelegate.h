e30b19ee90cff55fc2c252720bb49bb2e7b745e3
shiva475 <asiva345@gmail.com>
2018-04-16 17:01:10 +0530

    Initial Commit

---
+++ NewAppForPushing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewAppForPushing
+//
+//  Created by SmartRx-iOS on 16/04/18.
+//  Copyright © 2018 SmartRx-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewAppForPushing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewAppForPushing
+//
+//  Created by SmartRx-iOS on 16/04/18.
+//  Copyright © 2018 SmartRx-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
